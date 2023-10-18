06b639cebb00af2fa64f10aacffec5356e3b7a34
VitorDuarte303 <126691138+VitorDuarte303@users.noreply.github.com>
2023-10-18 22:15:40 +0100

    The Physics of AI
    2.Normalising Movement with Time

--- Assets/Script/SecondsUpdate.cs
+++ Assets/Script/SecondsUpdate.cs
@@ -8,6 +8,7 @@ public class SecondsUpdate : MonoBehaviour
 
     bool gotStartTime = false;
 
+    public float speed = 0.5f;
 
     void Update()
     {
@@ -18,6 +19,6 @@ public class SecondsUpdate : MonoBehaviour
         }
         this.transform.position = new Vector3(this.transform.position.x,
                                               this.transform.position.y,
-                                              Time.realtimeSinceStartup - timeStartOffset);
+                                              (Time.realtimeSinceStartup - timeStartOffset) * speed);
     }
 }
